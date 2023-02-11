9a897916e1208d1f4a0b3c6f945b1ebe363d364a
adarshUC <121399431+adarshUC@users.noreply.github.com>
2023-02-11 11:17:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH", "b8a814ed15eada43bc8c86d89d7f7618") #optional
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID", "1153171536")
 MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "5226602654:AAFrzzpACGxkamXIdVfNi8MXUc-pFFqyMPY")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
@@ -21,7 +21,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/adarshuc/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkANLCAypbZ38k8QKOnft4gkyyHLCAR4YtqOSl1XKgnj3PcT-SuoEjmzqEdg-c0yPKn-vDpnsU_Zw2vU47sB-9WaQzXBzbnBm_OlEyNTfU34toLBv4fLb7BnVrtjDaZyjT7krJxNbUHTxMLdNncyFAOvINJeGyIE3jzmM_uRHNd19XJSaXysMMXlGo8oh4U2hs7Es2aBWOY9MHdE2Ydji9wAR_otMepbHoE8i183AQEL-9U_IFmclUsiAtZa6tXVIE0CzLyk-dUDcZRQXNQGD-bCk0XNGwzRUKcvZ2noIKi4c_-3_vCMGL3Ly9kKc8NL1PomC2LIVWKCtqrKHyQtlA3IwAAAABEvABQAA")
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
